f552796ccfb6f29b4ca9645a58138c52bf12e7bd
Muhammad Zeeshan <79660812+ShaniDevelp@users.noreply.github.com>
2024-09-09 22:31:12 +0500

    portrfolio updated

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -37,10 +37,17 @@ const Portfolio = () => {
                                 <div className="content">
                                     <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
+                                    <div className="links">
                                     <button
                                         className="btn"
                                         onClick={() => window.open(port.url)}
                                     >View</button>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open('https://github.com')}
+                                    >GitHub</button>
+                                    </div>
+                                    
                                 </div>
                             </div>
                         )
