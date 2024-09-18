8a057848a8c508579bcc56cba300d73db1e6ab0d
Muhammad Zeeshan <79660812+ShaniDevelp@users.noreply.github.com>
2024-09-18 22:13:34 +0500

    new chnages

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -27,6 +27,18 @@ const Home = () => {
     '.',
   ]
 
+
+  const coder = {
+    name: 'Muhammad Zeeshan',
+    skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Full stack development', 'ReactJs', 'NextJs', 'NodeJs', 'ExpressJs', 'Postgress', 'MongoDB'],
+    hardWorker: true,
+    quickLearner: true,
+    problemSolver: true,
+    hireable: function () {
+      return this.hardWorker && this.problemSolver && this.skills.length >= 5;
+    }
+  };
+
   useEffect(() => {
     return setTimeout(() => {
       setLetterClass('text-animate-hover')
@@ -64,7 +76,56 @@ const Home = () => {
             CONTACT ME
           </Link>
         </div>
-        <Logo />
+        {/* <Logo /> */}
+        <div className="hero-section-code">
+          <div className="code-header">
+            <div className="code-dot yellow"></div>
+            <div className="code-dot green"></div>
+            <div className="code-dot pink"></div>
+          </div>
+          <div className="code-content">
+            <p className="code-line"><span className="code-keyword">const</span> <span className="code-variable">coder</span> = &#123;</p>
+            <div className="code-block">
+              <p>
+                <span className="code-property">name:</span> <span className="code-string">'{coder.name}'</span>,
+              </p>
+              <p>
+                <span className="code-property">skills:</span> [
+              </p>
+              <span className="code-skills">
+                {coder.skills.map((skill, index) => (
+                  <span key={index} className="code-skill">
+                    {skill}  {index < coder.skills.length - 1 && ', '}
+                  </span>
+                ))}
+              </span>
+              <p>],</p>
+              <p>
+                <span className="code-property">hardWorker:</span> <span className="code-boolean">{coder.hardWorker.toString()}</span>,
+              </p>
+              <p>
+                <span className="code-property">quickLearner:</span> <span className="code-boolean">{coder.quickLearner.toString()}</span>,
+              </p>
+              <p>
+                <span className="code-property">problemSolver:</span> <span className="code-boolean">{coder.problemSolver.toString()}</span>,
+              </p>
+              <p>
+                <span className="code-property">hireable:</span> <span className="code-function">function</span>() &#123;
+              </p>
+              <div className="code-block">
+                <p>return (</p>
+                <div className="code-block">
+                  <p className="code-return">this.<span>hardWorker</span> &&</p>
+                  <p className="code-return">this.<span>problemSolver</span> &&</p>
+                  <p className="code-return">this.<span>skills.length</span> &gt;= 5</p>
+                </div>
+                <p>);</p>
+              </div>
+              <p>&#125;,</p>
+            </div>
+            <p>&#125;;</p>
+          </div>
+        </div>
       </div>
 
       <Loader type="pacman" />
